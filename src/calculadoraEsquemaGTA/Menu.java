ea7dcf0dc60ecb6aa004be340f63b94c25acd42a
Thiago Gomes <thiagomello1999@hotmail.com>
2020-07-06 15:55:22 -0300

    primeiro commit

---
+++ src/calculadoraEsquemaGTA/Menu.java
@@ -0,0 +1,10 @@
+package calculadoraEsquemaGTA;
+
+/*calculadoraEsquemaGTA é uma aplicação com a finalidade de prover 
+projeções de ganhos em GTA$ ao realizar missões de esquema do modo online de GTA V*/
+
+public class Menu {
+	public static void main(String[] args) {
+
+	}
+}
